00ef1a33c4d1b9160286ffc3369a1530a3c7e58b
Brian Abeyta-Pratt <babeytapratt@gmail.com>
2020-12-03 11:56:57 -0700

    commit 2

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { useState, useEffect} from 'react'
+
+
+// development
+// production
+
+function url(path) {
+  return process.env.NODE_ENV === 'development'
+    ? `http://localhost:5000${path}`
+    : path
+}
+
+function App() {
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    fetch(url('/api/foo'))
+    .then(res => res.json())
+    .then(goods => setData(goods.data))
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {data}
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 const port = process.env.PORT || 4000
+const path = require('path')
 
 console.log('web 36 rocks')
 console.log(__dirname)
@@ -14,10 +16,19 @@ console.log(process.env.ALWAYS)
 console.log(process.env.LIKE)
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join__dirname, 'client/build'))
 
+// api, routers
 app.use('/api/*', (_, res) => {
     res.json({ data: 'web 36 rocks' })
 });
+
+app.use('*', (_, res) => {
+    // send back index.html
+    res.sendFile(path.join__dirname, 'client/build, index.html')
+})
 
 app.listen(port, () => {
     console.log(`listening on ${port}`)
